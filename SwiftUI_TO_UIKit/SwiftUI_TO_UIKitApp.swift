ed88888500e073ebdbb458b85e49c6ed6df19e5a
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-03-24 09:46:42 +0100

    Initial Commit

---
+++ SwiftUI_TO_UIKit/SwiftUI_TO_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TO_UIKitApp.swift
+//  SwiftUI_TO_UIKit
+//
+//  Created by Antonio Emanuele Cutarella on 24/03/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TO_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
