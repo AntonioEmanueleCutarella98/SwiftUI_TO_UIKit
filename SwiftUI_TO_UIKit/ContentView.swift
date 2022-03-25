edc1927e63185c1fe7869c20ce6e84024f414d31
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-03-25 09:59:34 +0100

    Added text to make the interface more understandable.

--- SwiftUI_TO_UIKit/ContentView.swift
+++ SwiftUI_TO_UIKit/ContentView.swift
@@ -15,29 +15,37 @@ struct ContentView: View {
     @State private var shouldPresentCamera = false
     
     var body: some View {
-        // WARNING: Force wrapped image for demo purpose
-        image!
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .frame(width: 200, height: 200)
-            .clipShape(Circle())
-            .overlay(Circle().stroke(Color.white, lineWidth: 4))
-            .shadow(radius: 10)
-            .onTapGesture { self.shouldPresentActionSheet = true }
-            .sheet(isPresented: $shouldPresentImagePicker) {
-                SUImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
-        }.actionSheet(isPresented: $shouldPresentActionSheet) { () -> ActionSheet in
-            ActionSheet(title: Text("Choose mode"), message: Text("Please choose your preferred mode to set your profile image"), buttons: [ActionSheet.Button.default(Text("Camera"), action: {
-                self.shouldPresentImagePicker = true
-                self.shouldPresentCamera = true
-            }), ActionSheet.Button.default(Text("Photo Library"), action: {
-                self.shouldPresentImagePicker = true
-                self.shouldPresentCamera = false
-            }), ActionSheet.Button.cancel()])
+
+        NavigationView {
+            VStack{
+                Text("Click on the image in the circle to add your photo!")
+                    .padding(.leading,27)
+                    .padding(.top,-150)
+            
+                image!
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 200, height: 200)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 10)
+                .onTapGesture { self.shouldPresentActionSheet = true }
+                .sheet(isPresented: $shouldPresentImagePicker) {
+                    SUImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
+            }.actionSheet(isPresented: $shouldPresentActionSheet) { () -> ActionSheet in
+                ActionSheet(title: Text("Choose mode"), message: Text("Please choose your preferred mode to set your photo"), buttons: [ActionSheet.Button.default(Text("Camera"), action: {
+                    self.shouldPresentImagePicker = true
+                    self.shouldPresentCamera = true
+                }), ActionSheet.Button.default(Text("Photo Library"), action: {
+                    self.shouldPresentImagePicker = true
+                    self.shouldPresentCamera = false
+                }), ActionSheet.Button.cancel()])
+            }
+            .navigationTitle("Photo Displayer")
+        }
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
