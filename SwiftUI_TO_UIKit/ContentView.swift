a5a0706cad9e5515b1e69866f203cf54afa3da00
AntonioEmanueleCutarella98 <antonioemanuele.cutarella@outlook.it>
2022-03-25 09:31:45 +0100

    I've added the ContentView to display the SUImagePicker, which is a function used to display the image picker or camera of the iPhone.

--- SwiftUI_TO_UIKit/ContentView.swift
+++ SwiftUI_TO_UIKit/ContentView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var image: Image? = Image(systemName: "photo.circle")
+    @State private var shouldPresentImagePicker = false
+    @State private var shouldPresentActionSheet = false
+    @State private var shouldPresentCamera = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // WARNING: Force wrapped image for demo purpose
+        image!
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 200, height: 200)
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+            .onTapGesture { self.shouldPresentActionSheet = true }
+            .sheet(isPresented: $shouldPresentImagePicker) {
+                SUImagePickerView(sourceType: self.shouldPresentCamera ? .camera : .photoLibrary, image: self.$image, isPresented: self.$shouldPresentImagePicker)
+        }.actionSheet(isPresented: $shouldPresentActionSheet) { () -> ActionSheet in
+            ActionSheet(title: Text("Choose mode"), message: Text("Please choose your preferred mode to set your profile image"), buttons: [ActionSheet.Button.default(Text("Camera"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = true
+            }), ActionSheet.Button.default(Text("Photo Library"), action: {
+                self.shouldPresentImagePicker = true
+                self.shouldPresentCamera = false
+            }), ActionSheet.Button.cancel()])
+        }
     }
 }
 
